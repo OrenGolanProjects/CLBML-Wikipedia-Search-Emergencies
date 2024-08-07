238b4d12efdcd40a8a8913c965cd6fe4933da412
Oren Golan <orenukistudy@gmail.com>
2024-08-07 19:59:00 +0300

    port update: updating the port to run in render

--- app.py
+++ app.py
@@ -177,4 +177,5 @@ def research():
                         auto_corr_results=auto_corr_results)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
